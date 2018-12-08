300709bcf365b843b25460a4693c47559d3ed70b
Mikael Ervum <36669674+Eonarion@users.noreply.github.com>
2018-12-08 17:53:28 +0100

    Update script.js

--- level1/question2/script.js
+++ level1/question2/script.js
@@ -1,25 +1,43 @@
 //Console.log out elements in JSON file
 
-// CHANGE THIS FOR LOOP TO WORK ON JSON DATA ARRAY
+// NOT DONE, RAN OUT OF TIME (might also not even be nearly correct, tried going over tutorials several times, was confused as to
+// how to correctly pull data from JSON file to JS, but as mentioned I ran out of time, and just had to deliver what I had so far)
 
-    for(var i=0; i < myJSON.length; i++){
+var request;
+if (window.XMLHttpRequest) {
+  request=new XMLHttpRequest();
+} else {
+  request=new ActiveXObject("Microsoft.XMLHTTP");
+}
 
 
-        if(theTest === false){
-            console.log(inputFields[i].name + " is incorrect")
+request.open('GET', 'author.json');
+
+request.onreadystatechange = function() {
+  if ((request.status === 200) &&
+  (request.readyState ===4)) {
+
+    var info = JSON.parse(request.responseText);
+
+    var output= '';
+    for (var i = 0; i <= info.video.author.data.length-1; i++) {
+      for (key in info.video.author.data[i]) {
+        if (info.video.author.data[i].hasOwnProperty(key)) {
+          console.log(info.video.author.data);
         }
+      }
     }
-})
 
+    var update = document.getElementByID('data');
+    update.innerHTML = output;
 
+  }
+}
 
+request.send();
 
-
-
-console.log(myJSON[0]);
 
 /* Reference guide to author.json file
-
 {
 	"video": [{
 		"id": 12312412312,
@@ -34,5 +52,4 @@ console.log(myJSON[0]);
 		}
 	}]
 }
-
 */
